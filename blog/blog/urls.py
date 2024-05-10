706a5a7a4005a3fb318eb213f6e3343781aa2272
Aleksander Kielczewski <olek.kielczewski@onet.pl>
2024-05-10 19:53:57 +0200

    dziala logowanie

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('przyklad/', include("user.urls")),
     path('', include('mainapp.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
